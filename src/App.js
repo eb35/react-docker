5f6b244ff7c5f57fc64eb23819b55260d84d2761
eb35 <roberthenry82@hotmail.com>
2022-09-16 00:29:06 -0500

    using env variable in react file

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-				<p>
-					Hello, World!
-				</p>
+				<p>Hi {`${process.env.REACT_APP_NAME}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
